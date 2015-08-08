350b60563167654a95619251949716916a1f3f90
Dave Shawley <daveshawley@gmail.com>
2015-08-08 07:41:33 -0400

    Move example test into examples directory.

---
+++ examples/__init__.py
@@ -0,0 +1,8 @@
+"""
+Examples of using glinda.
+
+This package contains examples that show how this package can be
+used.  These are included in the built documentation and used
+during testing to ensure that they are completely accurate.
+
+"""
